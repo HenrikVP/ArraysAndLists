29dd3fdaa06690721d9b988ea72d463694e4c05b
Henrik Vincents Poulsen <hpo@tec.dk>
2022-04-20 10:38:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ArraysAndLists;
+using NewProject;
+
+Skating.AddSkatingStuff();
+Skating.PrintArray();
+
+NewProjectClass npc1 = new NewProjectClass();
+//namespace ArraysAndLists
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//        }
+//    }
+
+
+//}
---
+++ Skating.cs
@@ -0,0 +1,41 @@
+﻿namespace ArraysAndLists
+{
+    internal static class Skating
+    {
+        private static string[] SkatingStuff = new string[5];
+        private static int counter;
+
+        public static void AddSkatingStuff()
+        {
+            SkatingStuff[0] = "Rulleskøjter";
+            SkatingStuff[1] = "Longboard";
+            SkatingStuff[2] = "Løbehjul";
+            SkatingStuff[3] = "Skateboard";
+            SkatingStuff[4] = "Strygebræt";
+            //SkatingStuff[5] = "IsThisEvenPossible?"; NO!
+
+        }
+
+
+        internal static void PrintArray()
+        {
+            foreach (var item in SkatingStuff)
+            {
+                Console.WriteLine(item);
+            }
+
+            for (int i = 0; i < SkatingStuff.Length; i++)
+            {
+                Console.WriteLine(SkatingStuff[i] + " " + i);
+            }
+
+
+            while (counter < SkatingStuff.Length)
+            {
+                Console.WriteLine(SkatingStuff[counter]);
+                counter++;
+            }
+        }
+
+    }
+}
